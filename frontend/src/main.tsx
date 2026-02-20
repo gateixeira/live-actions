deaf8444b52c2336ea2b0b3ece925a944822fff1
Guilherme Teixeira <4645845+gateixeira@users.noreply.github.com>
2026-02-20 17:55:34 +0100

    Fix css loading

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@primer/primitives/dist/css/functional/themes/light.css'
+import '@primer/primitives/dist/css/functional/themes/dark.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
